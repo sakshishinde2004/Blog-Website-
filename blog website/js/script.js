7cb49a48ded2a849e2c1f87a872499cad68e8833
sakshi shinde <143493678+sakshishinde2004@users.noreply.github.com>
2024-05-07 00:30:47 +0530

    Add files via upload

---
+++ blog website/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Select the .content element
+    const content = document.querySelector('.content');
+    
+    // Add class 'active' to trigger animation
+    content.classList.add('active');
+});
